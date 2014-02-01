c38627c351133ad70fa4ed585152c5ea5180e140
Sylvain Lobry <sylvainlobry@gmail.com>
2014-02-01 19:53:00 +0100

    Started the skeleton using the minimizer.
    	* src/main.cc: Here.

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,10 @@
 #include <iostream>
+
+#include <cv.h>
+#include <highgui.h>
+
 #include "Args.hh"
+#include "TVL0DecompositionMinimizer.hh"
 
 int main (int argc, char* argv[])
 {
@@ -12,5 +17,29 @@ int main (int argc, char* argv[])
     }
 
     std::cout << args << std::endl;
+
+    TVL0DecompositionMinimizer minimizer(args.getBetaBV(), args.getBetaS());
+
+    if (args.getWindowMode())
+    {
+	//FIX ME
+	std::cerr << "Window mode not handled." << std::endl;
+	return (1);
+    }
+    else
+    {
+	cv::Mat input = cv::imread(args.getInputImage(), CV_LOAD_IMAGE_GRAYSCALE);
+	if (minimizer.compute(input))
+	{
+	    cv::imwrite(args.getOutputImageBV(), minimizer.getOutputBV());
+	    cv::imwrite(args.getOutputImageS(), minimizer.getOutputS());
+	}
+	else
+	{
+	    std::cerr << "Something wrong happened during computation." << std::endl;
+	    return (1);
+	}
+    }
+
     return (0);
 }
