fcd48e7ed8499385b38b8833a6fe3f988c8bb113
Sylvain Lobry <sylvainlobry@gmail.com>
2014-02-01 17:31:04 +0100

    Uses the new class to check options.
         * src/main.cc: Here.

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,16 @@
 #include <iostream>
- 
+#include "Args.hh"
+
 int main (int argc, char* argv[])
 {
+    Args args(argc, argv);
+    if (!args.checkConsistency())
+    {
+	std::cerr << "Error: in non-WindowMode, you have to define input and output images." << std::endl;
+	args.printHelp();
+	return (1);
+    }
+
+    std::cout << args << std::endl;
     return (0);
 }
