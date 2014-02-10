de7c7572f37ebf75a4aaae36ccf18539bf3993d9
Sylvain Lobry <sylvainlobry@gmail.com>
2014-02-10 15:08:44 +0100

    Implements the graph construction
    	   * src/TVL0DecompositionMinimizer.cc: Here.

--- src/TVL0DecompositionMinimizer.cc
+++ src/TVL0DecompositionMinimizer.cc
@@ -32,25 +32,106 @@ TVL0DecompositionMinimizer::compute(const cv::Mat& input)
     //Allocation of the output images.
     if (OutputBV_)
 	delete OutputBV_;
-    OutputBV_ = new cv::Mat(input.size(), CV_8UC1);
+    OutputBV_ = new cv::Mat(input.size(), CV_64F);
 
     if (OutputS_)
 	delete OutputS_;
-    OutputS_ = new cv::Mat(input.size(), CV_8UC1);
+    OutputS_ = new cv::Mat(input.size(), CV_64F);
 
-    //Allocation of the memory for the graph.
-    unsigned nbPix = input.size().width * input.size().height;
+    //Useful information about the image.
+    unsigned Height = input.size().height;
+    unsigned Width = input.size().width;
+    unsigned nbPix = Width * Height;
     unsigned nbAlpha = Alpha_.size();
     unsigned nbNodes = nbPix * nbAlpha;
 
-    Graph* g = new Graph(nbNodes, 2 * nbNodes);
+    //Allocation of the memory for the graph.
+    Graph g (nbNodes, 2 * nbNodes);
     Graph::node_id* nodes = new Graph::node_id[nbNodes];
 
     //Creation of the nodes.
     for (unsigned i = 0; i < nbNodes; ++i)
-	nodes[i] = g->add_node();
+	nodes[i] = g.add_node();
 
-    return false;
+    //Links definition
+    for (unsigned i = 0; i < Height; ++i)
+    {
+	unsigned offset = i * Width;
+	for (unsigned j = 0; j < Width; ++j)
+	{
+	    /*For each site, we have one node for each possible value it can
+	      have.*/
+	    unsigned current = offset + j;
+
+	    ///////////////////////////////////
+            // Links defintion for data term //
+            ///////////////////////////////////
+
+	    //Smallest alpha is linked to the sink with infinite weight.
+	    g.add_tweights(nodes[current], 0, INFTY);
+
+	    //Every level (except last one) is linked to the one above it.
+	    for (unsigned level = 1; level < nbAlpha; ++level)
+		g.add_edge(nodes[current + (level - 1) * nbPix],
+			   nodes[current + level * nbPix],
+			   INFTY,
+			   42);//FIX ME
+
+	    //Last one is linked to the source
+	    g.add_tweights(nodes[current + (nbAlpha - 1) * nbPix], 42, 0);//FIX ME
+
+	    //////////////////////////////////////////////
+            // Links definition for regularization term //
+            //////////////////////////////////////////////
+	    //Eastern neighbour
+	    if (j < Width - 1)
+		for (unsigned level = 1; level <= nbAlpha; ++level)
+		{
+		    double beta = BetaBV_ * (Alpha_[level] - Alpha_[level - 1]);
+		    g.add_edge(nodes[current + (level - 1) * nbPix],
+			       nodes[current + (level - 1) * nbPix + 1],
+			       beta, beta);
+		}
+
+	    //Southern neighbour
+	    if (i < Height - 1)
+		for (unsigned level = 1; level <= nbAlpha; ++level)
+		{
+		    double beta = BetaBV_ * (Alpha_[level] - Alpha_[level - 1]);
+		    g.add_edge(nodes[current + (level - 1) * nbPix],
+			       nodes[current + (level - 1) * nbPix + Height],
+			       beta, beta);
+		}
+	}
+    }
+
+    //Computation of max flow.
+    g.maxflow();
+
+    //Construction of the ouput image.
+    for (unsigned i = 0; i < Height; ++i)
+    {
+	unsigned offset = i * Width;
+	for (unsigned j = 0; j < Width; ++j)
+	{
+	    unsigned current = offset + j;
+	    OutputBV_->at<double>(i, j) = 1;
+
+	    for (unsigned level = nbAlpha; level >= 1; --level)
+	    {
+		if (g.what_segment(nodes[current + (level - 1) * nbPix])
+		    != Graph::SOURCE)
+		{
+		    OutputBV_->at<double>(i, j) = Alpha_[level];
+		    break;
+		}
+	    }
+	}
+    }
+
+    delete nodes;
+
+    return true;
 }
 
 void
