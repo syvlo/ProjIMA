6c9af9b259e186216076088f405cc04d3890bc19
Sylvain Lobry <sylvainlobry@gmail.com>
2014-02-20 00:03:00 +0100

    Print value hoovered by mouse in the viewer.
          * src/viewer.cc: Here.

--- src/viewer.cc
+++ src/viewer.cc
@@ -5,6 +5,9 @@
 
 #include "ImwHelper.hh"
 
+#define WIN_NAME ("viewer")
+#define BORDER (3)
+
 void printHelp()
 {
     std::cout << "./Viewer image1 [image2]" << std::endl
@@ -12,6 +15,46 @@ void printHelp()
 	      << std::endl
 	      << "If 2 images, the first one will be used to compute mu+3sigma."
 	      << std::endl;
+}
+
+struct data
+{
+    cv::Mat image8U;
+    cv::Mat image8UOriginal;
+    cv::Mat image1;
+    cv::Mat image2;
+    bool doubleImage;
+};
+
+void MouseCallBack(int event, int x, int y, int flags, void* userdata)
+{
+    struct data* Data = (struct data*)userdata;
+    Data->image8U = Data->image8UOriginal.clone();
+    if (event == cv::EVENT_MOUSEMOVE)
+    {
+	if (!Data->doubleImage)
+	{
+	    char buffer[8];
+	    sprintf(buffer,"%d", (short)Data->image1.at<short> (y, x));
+	    cv::putText(Data->image8U, buffer, cv::Point(10, 15), 0, 0.3, 255);
+	    cv::imshow(WIN_NAME, Data->image8U);
+	}
+	else
+	{
+	    char buffer[8];
+	    buffer[0] = 0;
+	    if (x < Data->image1.size().width)
+	    {
+		sprintf(buffer,"%d", (short)Data->image1.at<short> (y, x));
+	    }
+	    if (x > Data->image1.size().width + BORDER)
+	    {
+		sprintf(buffer,"%d", (short)Data->image2.at<short> (y, x - Data->image1.size().width - BORDER));
+	    }
+	    cv::putText(Data->image8U, buffer, cv::Point(10, 15), 0, 0.3, 255);
+	    cv::imshow(WIN_NAME, Data->image8U);
+	}
+    }
 }
 
 int main (int argc, char* argv[])
@@ -46,19 +89,28 @@ int main (int argc, char* argv[])
 	if (img2_8U.size().height > height)
 	    height = img2_8U.size().height;
 
-	unsigned width = img1_8U.size().width + img2_8U.size().width + 3;
+	unsigned width = img1_8U.size().width + img2_8U.size().width + BORDER;
 	wholeImage = cv::Mat(height, width, CV_8U);
 
 	cv::Mat left(wholeImage, cv::Rect(0, 0, img1_8U.size().width,
 					  img1_8U.size().height));
 	img1_8U.copyTo(left);
-	cv::Mat right(wholeImage, cv::Rect(img1_8U.size().width + 3, 0,
+	cv::Mat right(wholeImage, cv::Rect(img1_8U.size().width + BORDER, 0,
 					   img2_8U.size().width,
 					   img2_8U.size().height));
 	img1_8U.copyTo(right);
     }
 
-    cv::imshow("Viewer", wholeImage);
+    struct data userData;
+    userData.image8U = wholeImage;
+    userData.image8UOriginal = wholeImage;
+    userData.image1 = img1;
+    userData.image2 = img2;
+    userData.doubleImage = (argc == 3);
+
+    cv::namedWindow(WIN_NAME, 1);
+    cv::setMouseCallback(WIN_NAME, MouseCallBack, &userData);
+    cv::imshow(WIN_NAME, wholeImage);
     cv::waitKey(0);
 
     return (0);
